20334c5ecbabc0572c42d42207f6401832c6e803
Ola <o.vinogradova@devcaz.com>
2026-08-28 15:53:18 +0200

    DTO models

---
+++ AutoTestsForApplication/DTO/DapperDTO/OrderItemDTO.cs
@@ -0,0 +1,10 @@
+namespace apitest.DTO.DapperDTO;
+
+public class OrderItemDTO
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
