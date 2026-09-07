2301b1151b70095eda14287ba350c3809aeb6381
Ola <o.vinogradova@devcaz.com>
2026-09-07 18:02:20 +0200

    BookStore DTO + IBookStore

---
+++ AutoTestsForApplication/DTO/BookStoreDTO/BooksListDTO.cs
@@ -0,0 +1,6 @@
+namespace apitest.DTO.BookStoreDTO;
+
+public class BooksListDTO
+{
+    public List<BookDTO> Books { get; set; }
+}
--- AutoTestsForApplication/Interfaces/IBookStore/IBookStore.cs
+++ AutoTestsForApplication/Interfaces/IBookStore/IBookStore.cs
@@ -10,4 +10,25 @@ public interface IBookStore
     
     [Post ("/Account/v1/GenerateToken")]
     Task<GetTokenDTO> GenerateTokenAsync([Body] LoginRequestDTO login);
+    
+    [Post("/Account/v1/Login")]
+    Task<LoginUserResponseDTO> LoginUserAsync([Body] UserDTO user);
+
+    [Post("/BookStore/v1/Books")]
+    Task<AddBookResponseDTO> AddBookAsync([Body] AddBookRequestDTO book,
+        [Header("Authorization")] string token);
+
+    [Get("/BookStore/v1/Books")]
+    Task<BooksListDTO> GetAllBookAsync();
+
+    [Get("/BookStore/v1/Book")]
+    Task<BookDTO> GetBookByIsbnAsync([Query] string ISBN);
+
+    [Delete("/BookStore/v1/Book")]
+    Task DeleteBookByIsbnAndByUserIdAsync([Body] DeleteBookRequestDTO book,
+        [Header("Authorization")] string token);
+    //при успешном удалении сервер отдает 204 no content, данная модель не нужна <DeleteBookResponseDTO>
+
+    [Get("/Account/v1/User/{UUID}")]
+    Task<AddBookResponseDTO> GetUserAsync(string UUID, [Header("Authorization")] string token);
 }
